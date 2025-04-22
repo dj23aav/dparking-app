04dc11459ebb70401717707c619c317009854e45
dj23aav <dj23aav@herts.ac.uk>
2025-04-22 21:35:36 +0100

    connecting esp32 to firebase
    connecting esp32 to firebase :  successfully implemented. if the switch is toggled on, the available_spot turns to true else false , which implies the car is occupied the space or not.

--- Esp32/Esp32.ino
+++ Esp32/Esp32.ino
@@ -1,7 +1,13 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 
+// Wi-Fi credentials
 const char* ssid = ""; //SSID
 const char* password = ""; //PWSD
+
+// Firebase Realtime Database
+const String firebaseHost = ""; // firebase Host
+const String firebasePath = ""; // firebase Path
 
 // Button setup
 const int buttonPin = 4;
@@ -10,15 +16,15 @@ int lastButtonState = HIGH;
 int toggleState = 0;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
+unsigned long lastReadTime = 0;
+unsigned long readInterval = 5000; // every 5 seconds
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  // Setup button pin
   pinMode(buttonPin, INPUT_PULLUP);
 
-  // Connect to WiFi
   Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -26,7 +32,7 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("");
+  Serial.println();
   Serial.println("WiFi connected!");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -35,7 +41,7 @@ void setup() {
 void loop() {
   int reading = digitalRead(buttonPin);
 
-  // Debounce logic
+  // Debounce
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
@@ -43,12 +49,68 @@ void loop() {
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading == LOW && buttonState == HIGH) {
       toggleState = !toggleState;
-     // Serial.println(toggleState);
+
       Serial.println(String(toggleState) + ": " + (toggleState ? "Available" : "Unavailable"));
 
+      updateFirebase(toggleState);  // Send update to Firebase
     }
     buttonState = reading;
   }
 
   lastButtonState = reading;
+
+  // Periodically read from Firebase
+  if (millis() - lastReadTime >= readInterval) {
+    readFromFirebase();  // Read and print available_spot
+    lastReadTime = millis();
+  }
+}
+
+void updateFirebase(bool availability) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = firebaseHost + firebasePath;
+
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonData = availability ? "true" : "false";
+
+    int httpResponseCode = http.PUT(jsonData);
+
+    if (httpResponseCode > 0) {
+      Serial.print("Firebase update successful: ");
+      Serial.println(httpResponseCode);
+    } else {
+      Serial.print("Error updating Firebase: ");
+      Serial.println(http.errorToString(httpResponseCode).c_str());
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi not connected");
+  }
+}
+
+void readFromFirebase() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = firebaseHost + firebasePath;
+
+    http.begin(url);
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.print("Current value of available_spot: ");
+      Serial.println(payload);
+    } else {
+      Serial.print("Error reading from Firebase: ");
+      Serial.println(http.errorToString(httpResponseCode).c_str());
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi not connected");
+  }
 }
